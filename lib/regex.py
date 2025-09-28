28813e81df42fd28c7459027774f3bba6643de7b
Beatrice Wambui <bettyneema2@gmail.com>
2025-09-28 18:42:54 +0300

    Implements regex patterns for name, phone, and email validations

--- lib/regex.py
+++ lib/regex.py
@@ -3,11 +3,31 @@ import re
 # NOTE: There are only a few tests included, so multiple solutions will work.
 # Feel free to encourage students to find oversights and add tests to this lab!
 
-name = r""
+import re
+
+#matches names like 
+#John Cena
+#Anya Taylor-Joy
+#D'Angelo
+#Mary-Kate O'Neill
+# - Capital letter, then lowercase letters
+# - A space
+# - Another capital letter, then lowercase letters
+name = r"^[A-Z][a-z]*(?:['-][A-Z][a-z]*)*(?: [A-Z][a-z]*(?:['-][A-Z][a-z]*)*)*$"
 name_regex = re.compile(name)
 
-phone_number = r""
+#phone numbers:
+# - (123) 456-7890
+# - 123-456-7890
+# - 5555555555
+phone_number = r"^(\(\d{3}\)\s?\d{3}-\d{4}|\d{3}-\d{3}-\d{4}|\d{10})$"
 phone_regex = re.compile(phone_number)
 
-email_address = r""
+
+#matches emails like:
+#john.cena@wwe.com 
+#first.last@domain.co.uk
+#123john.cena@wwe.com 
+email_address = r"^[A-Za-z][A-Za-z0-9._%+-]*@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$"
 email_regex = re.compile(email_address)
+
